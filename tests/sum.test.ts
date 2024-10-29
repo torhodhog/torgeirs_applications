73d8bc9b1c1832ef4e4efac605c0794dedb6771e
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-10-29 14:24:44 +0100

    Fjernet test

--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -1,6 +1,6 @@
-// tests/sum.test.ts
-import { sum } from '../sum';
+// // tests/sum.test.ts
+// import { sum } from '../sum';
 
-test('sums two numbers correctly', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+// test('sums two numbers correctly', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
